a99ccbbcf74a20e63cc919fbcb4b6c2ec15198e3
Noemi-Bovio <98837429+Noemi-Bovio@users.noreply.github.com>
2023-09-24 23:07:50 +0000

    pop up

--- javascript.js
+++ javascript.js
@@ -1,3 +1,23 @@
+// Funzione per mostrare il pop-up
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+// Funzione per chiudere il pop-up
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+// Mostra il pop-up quando il documento è pronto
+document.addEventListener("DOMContentLoaded", function() {
+  showPopup();
+});
+
+// Chiudi il pop-up quando l'utente clicca sul pulsante di chiusura
+document.getElementById("close-popup").addEventListener("click", function() {
+  closePopup();
+})
+
 var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       effect: "fade",
