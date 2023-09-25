82a9a7a9b0ee230d1b0b80f82a8c5bd76fcc3f68
Noemi-Bovio <98837429+Noemi-Bovio@users.noreply.github.com>
2023-09-25 16:57:23 +0000

    cookie

--- javascript.js
+++ javascript.js
@@ -28,4 +28,17 @@ function svuotaCarrello() {
   carrello.innerHTML = 0;
 }
 
+//freccina top
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
 
+//cookie
+function showCookie() {
+let cookie = document.querySelector('#cookie');
+cookie.style = "display:block;";
+}
+function hideCookie() {
+let cookie = document.querySelector('#cookie');
+cookie.style = "display:none;";
+}
